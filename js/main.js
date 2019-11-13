642e1ddc05a5c6612f3bd89447e01aad5378d899
omar <omar@MacBook-Pro-de-omar.local>
2019-11-13 11:16:06 +0100

    map fish slider fixes

--- js/main.js
+++ js/main.js
@@ -3,34 +3,34 @@
  */
 
 window.addEventListener(
-  'click',
+  "click",
   ev => {
-    const elm = ev.target
-    const attribute = elm.getAttribute('class')
+    const elm = ev.target;
+    const attribute = elm.getAttribute("class");
     if (
-      attribute === 'menu-mobile' ||
-      attribute === 'menu-mobile-icon-bar' ||
-      attribute === 'menu-item-mobile' ||
-      attribute === 'menu-mobile-button'
+      attribute === "menu-mobile" ||
+      attribute === "menu-mobile-icon-bar" ||
+      attribute === "menu-item-mobile" ||
+      attribute === "menu-mobile-button"
     ) {
-      const selector = '.collapse.first'
-      collapse(selector, 'toggle')
+      const selector = ".collapse.first";
+      collapse(selector, "toggle");
     }
   },
   false
-)
+);
 
 const fnmap = {
-  toggle: 'toggle',
-  show: 'add',
-  hide: 'remove'
-}
+  toggle: "toggle",
+  show: "add",
+  hide: "remove"
+};
 const collapse = (selector, cmd) => {
-  const targets = Array.from(document.querySelectorAll(selector))
+  const targets = Array.from(document.querySelectorAll(selector));
   targets.forEach(target => {
-    target.classList[fnmap[cmd]]('show')
-  })
-}
+    target.classList[fnmap[cmd]]("show");
+  });
+};
 
 /**
  * Reservation button fixed
@@ -38,52 +38,55 @@ const collapse = (selector, cmd) => {
 
 function addSlider() {
   document
-    .getElementById('fixed-button-container')
-    .classList.add('slide-right-resa-button')
+    .getElementById("fixed-button-container")
+    .classList.add("slide-right-resa-button");
 }
 
 // Get the container element
-var btnContainer = document.getElementsByClassName('menu-items')[0]
-console.log(btnContainer)
+var btnContainer = document.getElementsByClassName("menu-items")[0];
+console.log(btnContainer);
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName('menu-item')
+var btns = btnContainer.getElementsByClassName("menu-item");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function() {
-    var current = document.getElementsByClassName('menu-item-active')
-    current[0].className = current[0].className.replace(' menu-item-active', '')
-    this.className += ' menu-item-active'
-  })
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("menu-item-active");
+    current[0].className = current[0].className.replace(
+      " menu-item-active",
+      ""
+    );
+    this.className += " menu-item-active";
+  });
 }
 
-window.addEventListener('click', ev => {
-  const element = ev.target
-  const attribute = element.getAttribute('class')
-  if (attribute === 'slider-fleche') {
+window.addEventListener("click", ev => {
+  const element = ev.target;
+  const attribute = element.getAttribute("class");
+  if (attribute === "slider-fleche") {
     //left = getComputedStyle(element)
-    const figure = document.getElementsByClassName('slider-figure')
-    console.log(getComputedStyle(figure[0]).left)
-    const leftS = getComputedStyle(figure[0]).left
-    let regex = /[+-]?\d+(?:\.\d+)?/g
+    const figure = document.getElementsByClassName("slider-figure");
+    console.log(getComputedStyle(figure[0]).left);
+    const leftS = getComputedStyle(figure[0]).left;
+    let regex = /[+-]?\d+(?:\.\d+)?/g;
 
-    let leftInt = regex.exec(leftS)[0]
-    console.log(leftInt)
+    let leftInt = regex.exec(leftS)[0];
+    console.log(leftInt);
     if (leftInt === 0) {
       figure[0].className = figure[0].className.replace(
-        ' init-left',
-        ' left-100'
-      )
+        " init-left",
+        " left-100"
+      );
     } else if (leftInt === -610) {
       figure[0].className = figure[0].className.replace(
-        ' left-100',
-        ' left-200'
-      )
+        " left-100",
+        " left-200"
+      );
     } else {
       figure[0].className = figure[0].className.replace(
-        ' left-200',
-        ' init-left'
-      )
+        " left-200",
+        " init-left"
+      );
     }
   }
-})
+});
